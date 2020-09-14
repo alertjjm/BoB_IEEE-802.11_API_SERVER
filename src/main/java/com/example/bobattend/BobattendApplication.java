84f2b841ee3b91b94c423d08a395485b7e68f7e5
alertjjm <sdr2936@gmail.com>
2020-09-14 14:18:38 +0900

    [INITIAL] Initial commit

---
+++ src/main/java/com/example/bobattend/BobattendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bobattend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobattendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobattendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bobattend/BobattendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bobattend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobattendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
