50d91ba042c46ac24754b627d26ef67f6f6c421d
alertjjm <sdr2936@gmail.com>
2020-09-21 21:34:02 +0900

    [DELETE] Annotation deprecated

--- src/main/java/com/example/bobattend/Dto/AttendanceDto.java
+++ src/main/java/com/example/bobattend/Dto/AttendanceDto.java
@@ -1,14 +1,8 @@
 package com.example.bobattend.Dto;
-
-import com.example.bobattend.Entity.Attendance;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
