aed68e648a49d4e0375039d4275e8c5689e9b9fe
alertjjm <sdr2936@gmail.com>
2020-10-03 21:40:00 +0900

    [ADD] Https

--- src/main/java/com/example/bobattend/BobattendApplication.java
+++ src/main/java/com/example/bobattend/BobattendApplication.java
@@ -2,11 +2,14 @@ package com.example.bobattend;
 
 import com.example.bobattend.Dto.MemberDto;
 import com.example.bobattend.Service.MemberService;
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -37,5 +40,17 @@ public class BobattendApplication {
         characterEncodingFilter.setForceEncoding(true);
         return characterEncodingFilter;
     }
+    @Bean
+    public ServletWebServerFactory serveltContainer(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+    private Connector createStandardConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8070);
+        return connector;
+    }
+
 
 }
