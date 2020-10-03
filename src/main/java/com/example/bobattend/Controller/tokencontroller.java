1f11b26346240918f29d3f03e3d2a377ecbf1989
alertjjm <sdr2936@gmail.com>
2020-10-03 23:52:55 +0900

    [ADD] Https

--- src/main/java/com/example/bobattend/Controller/tokencontroller.java
+++ src/main/java/com/example/bobattend/Controller/tokencontroller.java
@@ -4,10 +4,14 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/.well-known/acme-challenge")
+@RequestMapping("/.well-known/pki-validation")
 public class tokencontroller {
     @GetMapping(value ="/{token}")
     public String token(@PathVariable("token") String token)  {
-        return token;
+        String tk="7B38DFA523E26B51783AD79F9229278254D20B8E6D86A5AB32FB51FA168DFFCA\n" +
+                "comodoca.com\n" +
+                "abb854dbc2f8806";
+        return tk;
     }
 }
+//.well-known/pki-validation/E0F24DDDB9CD29501E8BD299F3C54495.txt
