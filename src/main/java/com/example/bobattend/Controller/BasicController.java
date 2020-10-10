7762ef5087456b311fe7cc07b86f7efde88de15e
alertjjm <sdr2936@gmail.com>
2020-10-10 16:07:22 +0900

    [FIX] Find all

--- src/main/java/com/example/bobattend/Controller/BasicController.java
+++ src/main/java/com/example/bobattend/Controller/BasicController.java
@@ -42,7 +42,7 @@ public class BasicController {
     @GetMapping(value="/all", produces = "application/json")
     public String showall(){
         List<Member> memberList;
-        memberList =userrepo.findTop50by();
+        memberList =userrepo.findAllByPersonalidBetween(0,50);
         UserListDto jsonResult=new UserListDto(memberList.size(), memberList);
         Gson gson=new Gson();
         String i=gson.toJson(jsonResult);
